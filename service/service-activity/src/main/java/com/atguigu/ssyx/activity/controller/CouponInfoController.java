0ae6ec1d97a352a3f3678c7f69fbf29ca35e2d8f
GitHub1002 <2508812594@qq.com>
2023-11-22 05:17:09 +0800

    更新优惠券功能

--- service/service-activity/src/main/java/com/atguigu/ssyx/activity/controller/CouponInfoController.java
+++ service/service-activity/src/main/java/com/atguigu/ssyx/activity/controller/CouponInfoController.java
@@ -1,9 +1,17 @@
 package com.atguigu.ssyx.activity.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.atguigu.ssyx.activity.service.CouponInfoService;
+import com.atguigu.ssyx.common.result.Result;
+import com.atguigu.ssyx.model.activity.CouponInfo;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -14,8 +22,65 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2023-11-15
  */
 @RestController
-@RequestMapping("/activity/coupon-info")
+@RequestMapping("admin/activity/couponInfo")
+@CrossOrigin
 public class CouponInfoController {
+
+    @Autowired
+    private CouponInfoService couponInfoService;
+
+//    url: `${api_name}/${page}/${limit}`,
+//    method: 'get'
+    public Result list (
+            @ApiParam(name = "page", value = "当前页码", required = true)
+            @PathVariable Long page,
+            @ApiParam(name = "limit", value = "每页记录数", required = true)
+            @PathVariable Long limit
+        ){
+        IPage<CouponInfo> pageModel = couponInfoService.selectList(page, limit);
+        return Result.ok(pageModel);
+    }
+
+    @ApiOperation(value = "新增优惠券")
+    @PostMapping("save")
+    public Result save(@RequestBody CouponInfo couponInfo){
+        couponInfoService.save(couponInfo);
+        return Result.ok(null);
+    }
+
+    @ApiOperation(value = "修改优惠券")
+    @PutMapping("update")
+    public Result updateById(@RequestBody CouponInfo couponInfo){
+        couponInfoService.updateById(couponInfo);
+        return Result.ok(null);
+    }
+
+    @ApiOperation(value = "删除优惠券")
+    @DeleteMapping("remove/{id}")
+    public Result remove (@PathVariable String id){
+        couponInfoService.removeById(id);
+        return Result.ok(null);
+    }
+
+    @ApiOperation(value = "根据id列表删除优惠券")
+    @DeleteMapping("batchRemove")
+    public Result batchRemove (@RequestBody List<String> idList){
+        couponInfoService.removeByIds(idList);
+        return Result.ok(null);
+    }
+
+    @GetMapping("get/{id}")
+    public Result get(@PathVariable Long id){
+        CouponInfo couponInfo = couponInfoService.getCouponInfo(id);
+        return Result.ok(couponInfo);
+    }
+
+//    url: `${api_name}/findCouponRuleList/${id}`,
+//    method: 'get'
+    @GetMapping("findCouponRuleList/{id}")
+    public Result findCouponRuleList(@PathVariable Long id){
+        return Result.ok(couponInfoService.findCouponRuleList(id));
+    }
 
 }
 
--- service/service-activity/src/main/java/com/atguigu/ssyx/activity/service/impl/CouponInfoServiceImpl.java
+++ service/service-activity/src/main/java/com/atguigu/ssyx/activity/service/impl/CouponInfoServiceImpl.java
@@ -1,10 +1,16 @@
 package com.atguigu.ssyx.activity.service.impl;
 
+import com.atguigu.ssyx.enums.CouponRangeType;
 import com.atguigu.ssyx.model.activity.CouponInfo;
 import com.atguigu.ssyx.activity.mapper.CouponInfoMapper;
 import com.atguigu.ssyx.activity.service.CouponInfoService;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -17,4 +23,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class CouponInfoServiceImpl extends ServiceImpl<CouponInfoMapper, CouponInfo> implements CouponInfoService {
 
+    @Override
+    public IPage<CouponInfo> selectList(Long page, Long limit) {
+        Page<CouponInfo> pageParam = new Page<>(page, limit);
+        IPage<CouponInfo> couponInfoPage = baseMapper.selectPage(pageParam, null);
+        List<CouponInfo> couponInfoList = couponInfoPage.getRecords();
+        couponInfoList.stream().forEach(item -> {
+            item.setCouponTypeString(item.getCouponType().getComment());
+            CouponRangeType rangeType = item.getRangeType();
+            if (rangeType != null) {
+                item.setRangeTypeString(rangeType.getComment());
+            }
+        });
+        return couponInfoPage;
+    }
+
+    @Override
+    public CouponInfo getCouponInfo(Long id) {
+        CouponInfo couponInfo = baseMapper.selectById(id);
+        couponInfo.setCouponTypeString(couponInfo.getCouponType().getComment());
+        if (couponInfo.getRangeTypeString() != null) {
+            couponInfo.setRangeTypeString(couponInfo.getRangeType().getComment());
+        }
+        return couponInfo;
+    }
+
+    @Override
+    public Map<String, Object> findCouponRuleList(Long id) {
+        return null;
+    }
 }
